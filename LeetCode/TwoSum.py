01a1cdcc8bf648fe7d48354fd88ab1f82422f4b5
Harun Bansode <harun.bansode@hotmail.com>
2023-03-14 14:08:03 +0530

    successfully completed TwoSum

--- LeetCode/TwoSum.py
+++ LeetCode/TwoSum.py
@@ -1,2 +1,15 @@
-def TwoSum(int: n) -> int:
-    return n
+from typing import List
+
+
+def twoSum(nums: List[int], target: int) -> List[int]:
+    indices = []
+    for i in range(0, len(nums)):
+        for j in range(i + 1, len(nums)):
+            if nums[i] + nums[j] == target:
+                indices.append(i)
+                indices.append(j)
+                return indices
+
+nums = [2, 7, 11 , 15]
+target = 17
+print(twoSum(nums, target))

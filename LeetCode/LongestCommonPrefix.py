25d3d6a2c9fde4812e3db36252ae9619bd462643
Harun Bansode <harun.bansode@hotmail.com>
2023-03-17 01:47:03 +0530

    To be solved

--- LeetCode/LongestCommonPrefix.py
+++ LeetCode/LongestCommonPrefix.py
@@ -3,14 +3,49 @@ from typing import List
 def longestCommonPrefix(strs: List[str]) -> str:
         check_list = str(strs[0][:2])
         for i in range(1, len(strs)):
-            if strs[i] >= 1:
-                 if strs[i][:2] == check_list:
-                  return check_list
+            if len(strs) == 1:
+                  return strs[0]
+            else:
+                  if strs[i][:2] == check_list:
+                        return check_list
         return ''
 
 #strs = ["flower","flow","flight"]
-strs = ["dog","racecar","car"]
+#strs = ["dog","racecar","car"]
+strs = ["a"]
 print(longestCommonPrefix(strs))
+
+
+
+
+# check_list = str(strs[0][:2])
+#         for i in range(1, len(strs)):
+#             if len(strs) == 1:
+#                   return strs
+#             else:
+#                   if strs[i][:2] == check_list:
+#                         return check_list
+#         return ''
+
+
+
+
+
+
+# check_list = str(strs[0][:2])
+#         for i in range(1, len(strs)):
+#             if len(strs) > 1:
+#                  if strs[i][:2] == check_list:
+#                   return check_list
+#         return ''
+
+
+
+
+# for i in range(1, len(strs)):
+#     if strs[i][:2] == check_list:
+#         return check_list
+# return ''
 
 
 

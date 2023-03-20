7c4bd9afbc8b7ef5d79e30eb6cb63d3477429588
Harun Bansode <harun.bansode@hotmail.com>
2023-03-20 15:00:02 +0530

    Burgers.py

---
+++ CodeChef/Burgers.py
@@ -0,0 +1,14 @@
+for i in range(int(input())):
+    a, b = map(int, input().split())
+    if a > b:
+        print(b)
+    else:
+        print(a)
+
+# t = int(input())
+# for i in range(t):
+#     a, b = map(int, input().split())
+#     if a > b:
+#         print(b)
+#     else:
+#         print(a)

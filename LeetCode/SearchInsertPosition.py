91ca50c408c12bd82cbe315629dffbe65f54e159
Harun Bansode <harun.bansode@hotmail.com>
2023-03-31 20:50:30 +0530

    SearchInsertPosition

---
+++ LeetCode/SearchInsertPosition.py
@@ -0,0 +1,12 @@
+from typing import List
+
+# def searchInsert(nums: List[int], target: int) -> int:
+#     for i in range(0, len(nums)):
+#         if target in nums:
+#             print('Yes')
+#         else:
+#             nums1 = nums.append(target)
+
+# nums = [1,3,5,6]
+# target = 17
+# print(searchInsert(nums, target))
